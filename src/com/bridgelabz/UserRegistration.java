ef588854892fcfdd169efef6e6029e22ba905127
Shri <Shri@DESKTOP-G7BJ0I8>
2022-12-16 12:19:51 +0530

    added: UC1 check if first name is valid or not

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,8 +1,31 @@
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	public static boolean checkValidFirstName(String fName) {
+		String fNameRegEx = "[A-z][a-z]{3,}";
+		Pattern p = Pattern.compile(fNameRegEx);
+		if(fName==null) {
+			return false;
+		}
+		Matcher m = p.matcher(fName);
+		return m.matches();
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("****Welcome to User Registration****");
+		
+		String fName="Twarit";
+		boolean isfNameValid= checkValidFirstName(fName);
+		if(isfNameValid) {
+			System.out.println(fName + " is valid");
+		}
+		else
+			System.out.println(fName + " is not valid");
+		
+		
 		
 	}
 
