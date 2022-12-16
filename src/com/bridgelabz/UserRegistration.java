667a9e1429b75c9c9cb593e8e0016d68a3ef3657
Shri <Shri@DESKTOP-G7BJ0I8>
2022-12-16 12:23:24 +0530

    added: UC2 check if last name is valid or not

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -3,27 +3,29 @@ package com.bridgelabz;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+//checking for last name if it is valid or not
 public class UserRegistration {
-	public static boolean checkValidFirstName(String fName) {
-		String fNameRegEx = "[A-z][a-z]{3,}";
-		Pattern p = Pattern.compile(fNameRegEx);
-		if(fName==null) {
+	public static boolean checkValidLastName(String lName) {
+		String lNameRegEx = "[A-z][a-z]{3,}";
+		Pattern p = Pattern.compile(lNameRegEx);
+		if(lName==null) {
 			return false;
 		}
-		Matcher m = p.matcher(fName);
+		Matcher m = p.matcher(lName);
 		return m.matches();
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("****Welcome to User Registration****");
 		
-		String fName="Twarit";
-		boolean isfNameValid= checkValidFirstName(fName);
-		if(isfNameValid) {
-			System.out.println(fName + " is valid");
+		String lName="Chokniwal";
+		boolean islNameValid= checkValidLastName(lName);
+		if(islNameValid) {
+			System.out.println(lName + " is valid");
 		}
 		else
-			System.out.println(fName + " is not valid");
+			System.out.println(lName + " is not valid");
 		
 		
 		
